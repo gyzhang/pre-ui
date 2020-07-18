1cdf329c4c2f93ad010d51b38b28ef1e2c967fa7
Kevin Zhang <xprogrammer@163.com>
2020-07-18 20:27:00 +0800

    修改依赖库的版本号

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -5,19 +5,19 @@
         <img src="https://gitee.com/li_haodong/picture_management/raw/master/pic/WechatIMG9.png" height="256" width="256" alt="pre系统logo">
       </div>
       <span>
-        <a href="https://gitee.com/li_haodong/pre" target="_blank">
-          <img src="https://img.shields.io/badge/Pre-1.1-green.svg" alt="Build Status">
+        <a href="https://gitee.com/xprogrammer/pre" target="_blank">
+          <img src="https://img.shields.io/badge/Pre-1.4-green.svg" alt="Build Status">
         </a>
-        <img src="https://img.shields.io/badge/spring--boot-2.1.6.RELEASE-green.svg" alt="spring-boot">
-        <img src="https://img.shields.io/badge/security-5.1.5-blue.svg" alt="security">
-        <img src="https://img.shields.io/badge/mybatis--plus-3.1.2-blue.svg" alt="mybatis-plus">
+        <img src="https://img.shields.io/badge/spring--boot-2.3.1.RELEASE-green.svg" alt="spring-boot">
+        <img src="https://img.shields.io/badge/security-5.3.3-blue.svg" alt="security">
+        <img src="https://img.shields.io/badge/mybatis--plus-3.3.2-blue.svg" alt="mybatis-plus">
       </span>
       <br>
       <span>
         <el-collapse v-model="activeNames">
           <el-collapse-item title="Pre RBAC权限管理系统" name="1">
-            <div>基于Spring Boot 2.1.6.RELEASE</div>
-            <div>基于Spring Security 5.1.5</div>
+            <div>基于Spring Boot 2.3.1.RELEASE</div>
+            <div>基于Spring Security 5.3.3</div>
           </el-collapse-item>
           <el-collapse-item title="Pre 完美的容器化支持" name="2">
             <div>支持docker部署</div>
@@ -34,10 +34,10 @@
             <div>基于注解 + Aop切面实现全方位日记记录系统</div>
             <div>基于Mybatis拦截器 + 策略模式实现数据权限控制</div>
           </el-collapse-item>
-          <el-collapse-item title="基本功能" name="4">
+          <el-collapse-item title="基本功能" name="5">
             <div>用户管理 、角色管理 、角色管理 、菜单管理 、部门管理 、社交账号管理</div>
             <div>岗位管理 、字典管理 、操作日志 、异常日志 、代码生成</div>
-            <div><a href="https://gitee.com/li_haodong/pre" target="_blank">详细介绍Pre </a></div>
+            <div><a href="https://gitee.com/xprogrammer/pre" target="_blank">详细介绍Pre </a></div>
           </el-collapse-item>
         </el-collapse>
       </span>
